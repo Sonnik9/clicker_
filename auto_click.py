bb369cb4ba614f644a20f0a2866e83b355fa5d27
kali <kali@kali.netis.cc>
2024-06-04 18:40:04 +0300

    f comm

---
+++ auto_click.py
@@ -0,0 +1,39 @@
+from pynput.keyboard import Key, Listener
+from pynput.mouse import Button, Controller
+import time
+
+cleck_interval = 60
+
+class CLICKERR():
+    def __init__(self) -> None:
+        self.isClicking = False
+        self.hotkey = Key.alt
+        self.mouse = Controller()
+
+    def toggle_clicker(self):
+        self.isClicking = not self.isClicking
+        if self.isClicking:
+            print("Кликер включен")            
+        else:
+            print("Кликер отключен")
+
+    def on_key_press(self, key):
+        if key == self.hotkey:
+            self.toggle_clicker()
+
+    def run_clicker(self):
+        while True:
+            if self.isClicking:
+                self.mouse.click(Button.left, 2)
+                time.sleep(cleck_interval)
+
+def main():
+    cl = CLICKERR()
+    print('Please tab Alt for start clicking and the same key for stoping!')
+    listener = Listener(on_press=cl.on_key_press)
+    listener.start()
+    cl.run_clicker()
+    listener.join()
+
+if __name__=="__main__":
+    main()
---
+++ click_tester.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+tk = Tk()
+tk.title('Clicker')
+tk.geometry("1000x550")
+n = 0
+
+def nplus():
+    global n
+    n = n + 1
+    label['text'] = str(n) + '$'
+
+def nsbros():
+    global n
+    n = 0
+    label['text'] = str(n) + '$'
+
+btn1 = Button(text="Click", background="#000", foreground="#fff",
+             padx="20", pady="8", font="16", command=nplus)
+btn1.pack()
+
+label = Label(tk, text=str(n)+'$', font=('Helvetica 100'))
+label.pack()
+btn2 = Button(text="Reset", background="#000", foreground="#fff",
+             padx="20", pady="8", font="16", command=nsbros)
+btn2.pack()
+mainloop()
